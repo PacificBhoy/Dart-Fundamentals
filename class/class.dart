b602701e986845f765b1bd283b435743ec16922d
PacificBhoy <isaacjuniorndukwe@gmail.com>
2026-05-14 10:37:34 +0100

    inheritance example

--- class/class.dart
+++ class/class.dart
@@ -3,13 +3,37 @@ class CarDetails {
   String make;
   String model;
   int year;
-  
+
   // Constructor to initialize the properties
   CarDetails(this.make, this.model, this.year);
- 
+
   // Method to display the details of the car
   void displayDetails() {
     print('Car: $make $model ($year)');
+  }
+}
+
+class User {
+  // Define a property for the username
+  String Username;
+  User(this.Username);
+
+  void login() {
+    // Simulate a user login by printing a message to the console
+    print("$Username has logged in");
+  }
+}
+
+class Admin extends User {
+  // Define a property for the admin level
+  int adminLevel;
+  Admin(String username, this.adminLevel) : super(username);
+
+  @override // Override the login method to include additional information about the admin level
+  void login() {
+    // Call the login method of the parent class (User) to maintain the base functionality
+    print("Security Alert: Admin $Username with level $adminLevel has logged in",
+    );
   }
 }
 
@@ -18,5 +42,11 @@ void main() {
   var myCar = CarDetails('Toyota', 'Camry', 2020);
 
   // Display the details of the car using the displayDetails method, which is a member function of the CarDetails class.
-  myCar.displayDetails(); 
+  myCar.displayDetails();
+
+  var user1 = User('Alice'); // Create an instance of the User class and call the login method
+  user1.login();
+
+  var admin1 = Admin('Bob', 5); // Create an instance of the Admin class and call the login method, which is overridden to include additional information about the admin level.
+  admin1.login();
 }
